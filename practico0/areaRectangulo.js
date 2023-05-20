f5115d0a7be1b304555035fd6c89bae0867f1330
Tania Masset <taniamasset@gmail.com>
2023-05-20 11:44:02 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,8 @@
+let base=12.5;
+let altura=5.3;
+
+
+let resultado=base * altura;
+console.log("El area del rectangulo es "+resultado);
+
+//el area del rectangulo con base 5 y altura 3 es 15
---
+++ practico0/promedio3.js
@@ -0,0 +1,11 @@
+let numero1=-1.2;
+let numero2=5.3;
+let numero3=-3.3;
+
+let resultado=(numero1 + numero2 + numero3)/3;
+console.log("El promedio es "+resultado);
+/*
+a. ¿Cuál es el promedio entre 5, 7 y 9? 7
+b. ¿Cuál es el promedio entre -2 , -3, -8? -4.33
+c. ¿Cuál es el promedio entre -1.2, 5.3, -3.3? 0.266
+*/
