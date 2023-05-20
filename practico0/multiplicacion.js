c6957ca69a87f163b6738df78945ffa5460632bd
Tania Masset <taniamasset@gmail.com>
2023-05-20 11:38:03 -0300

    agrego resta y multiplicacion

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,6 @@
+let numero1=2;
+let numero2=17;
+let numero3=6;
+
+let resultado=numero1 * numero2 * numero3;
+console.log("El resultado de la multiplicacionn es "+resultado);
---
+++ practico0/resta.js
@@ -0,0 +1,4 @@
+let minuendo=10;
+let sustraendo=2;
+let resultado=minuendo-sustraendo;
+console.log("El resultado de la resta es "+resultado);
