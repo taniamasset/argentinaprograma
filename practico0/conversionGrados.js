db2330bf14052cd523a31d1debb1eb062d94d3a7
Tania Masset <taniamasset@gmail.com>
2023-05-20 11:40:56 -0300

    agrego conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,7 @@
+let Fahrenheit=75;
+let celsius=(Fahrenheit-32)*(5/9);
+console.log( Fahrenheit+" grados Fahrenheit son "+celsius+" grados celsius")
+/* 
+a. ¿Cuántos grados C son 50 F? 10
+b. ¿Cuántos grados C son 75 F? 23.88
+*/
